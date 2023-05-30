474b2d88223244b6a787470c9d4bdef78eb44db3
Hugo <hugo@world.game>
2023-05-30 15:11:39 +0200

    added question_check tool

--- server/tools.py
+++ server/tools.py
@@ -2,7 +2,8 @@ from dotenv import load_dotenv
 from langchain.chains import RetrievalQA
 from langchain.embeddings import HuggingFaceEmbeddings, HuggingFaceInstructEmbeddings
 from langchain import HuggingFacePipeline
-
+from colorama import Fore, Style
+import re
 from langchain.vectorstores import Chroma
 from langchain.docstore.document import Document
 from langchain.text_splitter import CharacterTextSplitter, TokenTextSplitter, RecursiveCharacterTextSplitter
@@ -24,6 +25,23 @@ EMBEDDINGS_MAP = {
 
 CHROMA_SETTINGS = {}  # Set your Chroma settings here
 
+QUESTION_CHECK_PROMPT_TEMPLATE = """The context is:
+
+{}
+
+Based on this context, the question is: {}
+
+Can this question be answered with the given context?"""
+
+def clean_text(text):
+    # Remove line breaks
+    text = text.replace('\n', ' ')
+
+    # Remove special characters
+    text = re.sub(r'[^\w\s]', '', text)
+    
+    return text
+
 def load_unstructured_document(document: str) -> list[Document]:
     with open(document, 'r') as file:
         text = file.read()
@@ -33,6 +51,39 @@ def load_unstructured_document(document: str) -> list[Document]:
 def split_documents(documents: list[Document], chunk_size: int = 100, chunk_overlap: int = 0) -> list[Document]:
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
     return text_splitter.split_documents(documents)
+
+def checkQuestion(question: str, retriever):
+    QUESTION_CHECK_PROMPT_TEMPLATE = """You MUST answer with 'yes' or 'no'. Given the following pieces of context, determine if there are any elements related to the question in the context.
+Don't forget you MUST answer with 'yes' or 'no'.
+Context:{context}
+Question: Are there any elements related to ""{question}"" in the context?
+"""
+    llm = OobaboogaLLM()  # Initialize the LLM you use
+    qa = RetrievalQA.from_chain_type(llm=llm, chain_type="stuff", retriever=retriever, return_source_documents=True)
+
+    # Answer the question
+    answer_data = qa({"query": question})
+
+    # Check if 'answer' is in answer_data, if not print it in bold red
+    if 'result' not in answer_data:
+        print(f"\033[1;31m{answer_data}\033[0m")
+        return "Issue in retrieving the answer."
+
+    answer = answer_data['result']
+    context_documents = answer_data['source_documents']
+
+    # Combine all contexts into one
+    context = " ".join([clean_text(doc.page_content) for doc in context_documents])
+    # Formulate the prompt for the LLM
+    question_check_prompt = QUESTION_CHECK_PROMPT_TEMPLATE.format(context=context, question=question)
+    print(Fore.GREEN + Style.BRIGHT + question_check_prompt + Style.RESET_ALL)
+    # Submit the prompt to the LLM directly
+    answerable = llm.call_api(question_check_prompt)
+    print(Fore.GREEN + Style.BRIGHT + answerable + Style.RESET_ALL)
+    return answerable[-3:]
+
+
+
 
 def load_tools(llm_model):
     def ingest_file(file_path):
@@ -64,7 +115,14 @@ def load_tools(llm_model):
     retriever, title = ingest_file(file_path)
 
     def searchChroma(key_word):
-        hf_llm = OobaboogaLLM()  # commented out
+    # First, check if the question can be answered with the given context
+        check_result = checkQuestion(key_word, retriever)
+        print(Fore.GREEN + Style.BRIGHT + check_result + Style.RESET_ALL)
+        if check_result.lower() != "yes":
+            print("The question cannot be answered with the given context.")
+            return
+
+        hf_llm = OobaboogaLLM()  # Initialize your LLM
         qa = RetrievalQA.from_chain_type(llm=hf_llm, chain_type="stuff", retriever=retriever, return_source_documents=False)
 
         print(qa)
@@ -75,6 +133,7 @@ def load_tools(llm_model):
     dict_tools = {
         'Chroma Search': searchChroma,
         'File Ingestion': ingest_file,
+        'Check Question': checkQuestion,
     }
 
     return dict_tools
